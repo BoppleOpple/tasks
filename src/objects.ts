f38f43b23f491d16d2fff3ade397ee4f535eaa07
Liam Hillery <william.hillery@maine.edu>
2024-09-30 22:09:25 -0400

    object tests complete

--- src/objects.ts
+++ src/objects.ts
@@ -8,9 +8,18 @@ import { Question, QuestionType } from "./interfaces/question";
 export function makeBlankQuestion(
     id: number,
     name: string,
-    type: QuestionType
+    type: QuestionType,
 ): Question {
-    return {};
+    return {
+        id: id,
+        name: name,
+        type: type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false,
+    };
 }
 
 /**
@@ -21,7 +30,9 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    return (
+        question.expected.trim().toLowerCase() == answer.trim().toLowerCase()
+    );
 }
 
 /**
@@ -31,7 +42,13 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type == "short_answer_question") return true;
+
+    return question.options.reduce(
+        (previousValue: boolean, option: string): boolean =>
+            previousValue || option == answer,
+        false,
+    );
 }
 
 /**
@@ -41,7 +58,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return `${question.id}: ${question.name.slice(0, 10)}`;
 }
 
 /**
@@ -62,7 +79,29 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let markdownString: string = `# ${question.name}\n`;
+    markdownString += `${question.body}`;
+    if (question.type == "multiple_choice_question") {
+        markdownString += "\n- " + question.options.join("\n- ");
+    }
+    return markdownString;
+}
+/**
+ * Returns a deep copy of a Question object
+ * @param question the Question object to copy
+ */
+
+function deepCopy(question: Question): Question {
+    return {
+        id: question.id,
+        name: question.name,
+        type: question.type,
+        body: question.body,
+        expected: question.expected,
+        options: [...question.options],
+        points: question.points,
+        published: question.published,
+    };
 }
 
 /**
@@ -70,7 +109,9 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    let copy: Question = deepCopy(question);
+    copy.name = newName;
+    return copy;
 }
 
 /**
@@ -79,7 +120,9 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    let copy: Question = deepCopy(question);
+    copy.published = !question.published;
+    return copy;
 }
 
 /**
@@ -89,7 +132,11 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    let copy: Question = deepCopy(oldQuestion);
+    copy.id = id;
+    copy.name = `Copy of ${oldQuestion.name}`;
+    copy.published = false;
+    return copy;
 }
 
 /**
@@ -100,7 +147,9 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    let copy: Question = deepCopy(question);
+    copy.options.push(newOption);
+    return copy;
 }
 
 /**
@@ -115,7 +164,12 @@ export function mergeQuestion(
     id: number,
     name: string,
     contentQuestion: Question,
-    { points }: { points: number }
+    { points }: { points: number },
 ): Question {
-    return contentQuestion;
+    let copy: Question = deepCopy(contentQuestion);
+    copy.id = id;
+    copy.name = name;
+    copy.points = points;
+    copy.published = false;
+    return copy;
 }
